e1cdfede1efda6b062291ee2fef6557eca8b9242
Dias <dias.atudinov@gmail.com>
2025-07-28 10:41:13 +0500

    app settings set up

---
+++ Nugget Gamining/Nugget Gamining/Nugget_GaminingApp.swift
@@ -0,0 +1,16 @@
+//
+//  Nugget_GaminingApp.swift
+//  Nugget Gamining
+//
+//
+
+import SwiftUI
+
+@main
+struct Nugget_GaminingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
